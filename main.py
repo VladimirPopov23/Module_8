87dc3275ff3b0bb10ff0225a0d472b82310dc7a5
VladimirPopov23 <volodyaklever@gmail.com>
2024-11-16 17:09:24 +0300

    module_8_2.py
    16.11.2024 Задача "План перехват"

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
-# module_8_1.py
-# 13.11.2024 Задание "Программистам всё можно"
+# module_8_2.py
+# 16.11.2024 Задача "План перехват"
 
-def add_everything_up(a, b):
+def personal_sum(numbers):
+    result = 0
+    incorrect_data = 0
+    for i in numbers:
+        try:
+            result += i
+        except TypeError:
+            incorrect_data += 1
+            print(f'Некорректный тип данных для подсчёта суммы - {i}')
+    return result, incorrect_data
+
+
+def calculate_average(numbers):
     try:
-        c = a + b
-        return round(c, 3)
+        result, incorrect_data = personal_sum(numbers)
+        arithmetic_mean = result / (len(numbers) - incorrect_data)
+        return arithmetic_mean
+    except ZeroDivisionError:
+        return 0
     except TypeError:
-        return str(a) + str(b)
+        print(f'В numbers записан некорректный тип данных')
+        return None
 
-print(add_everything_up(123.456, 'строка'))
-print(add_everything_up('яблоко', 4215))
-print(add_everything_up(123.456, 7))
+
+
+print(f'Результат 1: {calculate_average("1, 2, 3")}')
+print(f'Результат 2: {calculate_average([1, "Строка", 3, "Ещё Строка"])}')
+print(f'Результат 3: {calculate_average(567)}')
+print(f'Результат 4: {calculate_average([42, 15, 36, 13])}')
